2e49c158b70df6b9a0e921514b4c02ce9bd158ea
Jyrone Parker <jyrone.parker@mgmail.com>
2021-09-24 18:07:11 -0400

    J Computer Solutions Logo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-webrtc width="100%" :roomId="roomId" ref="webrtc" />
+    <input v-model="roomId" placeholder="Enter room ID"/>
+    <button @click="toggleRoom">{{hasJoined ? 'Leave Room' : 'Join Room'}}</button>
+    <button @click="screenShare" v-if="hasJoined">Screen Share</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      roomId: 'roomId',
+      hasJoined: false
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+    toggleRoom () {
+      if(this.hasJoined) {
+        this.$refs.webrtc.leave()
+        this.hasJoined = false
+      } else {
+      this.$refs.webrtc.join()
+      this.hasJoined = true
+      }
+    },
+    screenShare () {
+      this.$refs.webrtc.shareScreen()
+    }
   }
 }
 </script>
