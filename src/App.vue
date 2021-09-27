5d8122fae1f5df41f7f68d92d1dc66a72145836d
Jyrone Parker <jyrone.parker@mgmail.com>
2021-09-27 03:02:15 -0400

    record

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <vue-webrtc width="100%" :roomId="roomId" ref="webrtc" />
+    <vue-webrtc width="100%" :roomId="roomId" ref="webrtc" v-on:share-started="shareStarted"  v-on:share-stopped="leftRoom" v-on:left-room="leftRoom" v-on:joined-room="joinedRoom"/>
     <input v-model="roomId" placeholder="Enter room ID"/>
     <button @click="toggleRoom">{{hasJoined ? 'Leave Room' : 'Join Room'}}</button>
     <button @click="screenShare" v-if="hasJoined">Screen Share</button>
+    <button @click="record" v-if="hasJoined">Record</button>
   </div>
 </template>
 
@@ -14,24 +15,77 @@ export default {
   data () {
     return {
       roomId: 'roomId',
-      hasJoined: false
+      hasJoined: false,
+      mediaRecorder: null,
+      chunks: [],
+      userStream: null
     }
   },
-  mounted () {
-
-  },
+  mounted () {},
   methods: {
-    toggleRoom () {
-      if(this.hasJoined) {
-        this.$refs.webrtc.leave()
-        this.hasJoined = false
-      } else {
-      this.$refs.webrtc.join()
-      this.hasJoined = true
+    onStop () {
+      var blob = new Blob(this.chunks, { 'type' : 'video/webm' }); // other types are available such as 'video/webm' for instance, see the doc for more info
+      this.chunks = [];
+      const file = new File ([blob], 'meeting.mp4', { 'type' : 'video/webm' })
+      console.log(file)
+      var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = 'meeting.webm';
+        document.body.appendChild(a);
+        a.click()
+    },
+    pushData (e) {
+      this.chunks.push(e.data);
+      console.log(e.data)
+    },
+    record () {
+      this.mediaRecorder.start()
+    },
+    async toggleRoom () {
+      try {
+        if(this.hasJoined) {
+          this.$refs.webrtc.leave()
+          this.hasJoined = false
+          this.mediaRecorder.stop()
+        } else {
+          await this.$refs.webrtc.join()
+          this.userStream = this.$refs.webrtc.videoList[0].stream
+          this.mediaRecorder = new MediaRecorder(this.userStream)
+          this.mediaRecorder.ondataavailable = e => this.pushData(e)
+          this.mediaRecorder.onstop = () => this.onStop()
+          this.hasJoined = true
+        }
+      } catch (e) {
+        console.log(e)
       }
+
     },
     screenShare () {
       this.$refs.webrtc.shareScreen()
+    },
+    addTrack(streamId) {
+      try {
+        console.log(streamId)
+        const streams = this.$refs.webrtc.videoList
+        console.log(streams)
+        streams.forEach(stream => {
+          console.log(stream.stream)
+          this.userStream.addTrack(stream.stream)
+        })
+      } catch (e) {
+          console.log(e)
+      }
+    },
+    joinedRoom (streamId) {
+      this.addTrack(streamId)
+    },
+    shareStarted (streamId) {
+      this.addTrack(streamId)
+    },
+    leftRoom (streamId) {
+      console.log(streamId)
+      // this.mediaRecorder.removeTrack(streamId)
     }
   }
 }
