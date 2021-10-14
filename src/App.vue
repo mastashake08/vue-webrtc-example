9c96be98c3da0c032f66aab23cd8ea2007e776ca
Jyrone Parker <jyrone.parker@mgmail.com>
2021-10-14 12:32:13 -0400

    add ads

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Adsense
+      data-ad-client="ca-pub-7023023584987784"
+      data-ad-slot="5070366491">
+    </Adsense>
+    <img alt="Vue logo" src="./assets/logo.png" class="animate-fade-slow object-contain h-80 w-full">
     <vue-webrtc width="100%" :roomId="roomId" ref="webrtc" v-on:share-started="shareStarted"  v-on:share-stopped="leftRoom" v-on:left-room="leftRoom" v-on:joined-room="joinedRoom"/>
     <input v-model="roomId" placeholder="Enter room ID"/>
-    <button @click="toggleRoom">{{hasJoined ? 'Leave Room' : 'Join Room'}}</button>
-    <button @click="screenShare" v-if="hasJoined">Screen Share</button>
-    <button @click="record" v-if="hasJoined">Record</button>
+    <button type="button" class="ml-5" @click="toggleRoom">{{hasJoined ? 'Leave Room' : 'Join Room'}}</button>
+    <button type="button" class="ml-5" @click="screenShare" v-if="hasJoined">Screen Share</button>
+    <!-- <button @click="record" v-if="hasJoined" v->Record</button> -->
   </div>
 </template>
 
@@ -26,7 +30,7 @@ export default {
     onStop () {
       var blob = new Blob(this.chunks, { 'type' : 'video/webm' }); // other types are available such as 'video/webm' for instance, see the doc for more info
       this.chunks = [];
-      const file = new File ([blob], 'meeting.mp4', { 'type' : 'video/webm' })
+      const file = new File ([blob], 'meeting.webm', { 'type' : 'video/webm' })
       console.log(file)
       var a = document.createElement("a"),
                 url = URL.createObjectURL(file);
@@ -55,6 +59,7 @@ export default {
           this.mediaRecorder.ondataavailable = e => this.pushData(e)
           this.mediaRecorder.onstop = () => this.onStop()
           this.hasJoined = true
+
         }
       } catch (e) {
         console.log(e)
@@ -64,28 +69,30 @@ export default {
     screenShare () {
       this.$refs.webrtc.shareScreen()
     },
-    addTrack(streamId) {
+    async addTrack(streamId) {
       try {
         console.log(streamId)
         const streams = this.$refs.webrtc.videoList
         console.log(streams)
         streams.forEach(stream => {
-          console.log(stream.stream)
-          this.userStream.addTrack(stream.stream)
+          console.log(stream.getTracks())
+          this.userStream.addTrack(stream)
         })
       } catch (e) {
           console.log(e)
       }
     },
     joinedRoom (streamId) {
-      this.addTrack(streamId)
+      // this.addTrack(streamId)
+      console.log(streamId)
     },
     shareStarted (streamId) {
-      this.addTrack(streamId)
+      console.log(streamId)
+      // this.addTrack(streamId)
     },
     leftRoom (streamId) {
-      console.log(streamId)
       // this.mediaRecorder.removeTrack(streamId)
+      console.log(streamId)
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,47 @@
 import Vue from 'vue'
 import App from './App.vue'
 import WebRTC from 'vue-webrtc'
+import Ads from 'vue-google-adsense'
+import "tailwindcss/tailwind.css"
+import VueTailwind from 'vue-tailwind'
 
+const settings = {
+  TInput: {
+    classes: 'form-input border-2 text-gray-700',
+    variants: {
+      error: 'form-input border-2 border-red-300 bg-red-100',
+      // ... Infinite variants
+    }
+  },
+TButton: {
+    classes: 'rounded-lg border block inline-flex items-center justify-center block px-4 py-2 transition duration-100 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed',
+    variants: {
+      secondary: 'rounded-lg border block inline-flex items-center justify-center bg-purple-500 border-purple-500 hover:bg-purple-600 hover:border-purple-600',
+    }
+  },
+  TAlert: {
+    classes: {
+      wrapper: 'rounded bg-blue-100 p-4 flex text-sm border-l-4 border-blue-500',
+      body: 'flex-grow text-blue-700',
+      close: 'text-blue-700 hover:text-blue-500 hover:bg-blue-200 ml-4 rounded',
+      closeIcon: 'h-5 w-5 fill-current'
+    },
+    variants: {
+      danger: {
+        wrapper: 'rounded bg-red-100 p-4 flex text-sm border-l-4 border-red-500',
+        body: 'flex-grow text-red-700',
+        close: 'text-red-700 hover:text-red-500 hover:bg-red-200 ml-4 rounded'
+      },
+      // ... Infinite variants
+    }
+  },
+  // ... The rest of the components
+}
+
+Vue.use(VueTailwind, settings)
+Vue.use(require('vue-script2'))
+
+Vue.use(Ads.Adsense)
 Vue.use(WebRTC)
 Vue.config.productionTip = false
 
